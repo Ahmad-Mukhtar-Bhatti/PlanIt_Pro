4efc8335b10c63d4af5073647917234c30cd3d88
Ahmad-Mukhtar-Bhatti <ahmadmukhtar2001@gmail.com>
2023-04-02 06:22:30 +0500

    backend connected

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,5 +1,7 @@
 import './login.css';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
 
 const img = require("./planitpro_logo.png");
 
@@ -8,6 +10,25 @@ const Login = (props) => {
     const Logininto = () => {
         console.log("Login handler")
     }
+
+    // Connecting the Login button with the backend
+    const [data, setData] = useState(null);
+    const navigate = useNavigate();
+    const handleClick = () => {
+      fetch('http://localhost:3010/api/data', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ message: 'Hello brother, I am connected!' })
+      })
+      .then(response => response.json())
+      .then(data => setData(data.message))
+      .catch(error => console.error(error));
+    };
+
+
+
     return (
 
         <body class = "li-body">
@@ -15,7 +36,7 @@ const Login = (props) => {
             <br></br> 
             <img src={img} alt="PlanIt Pro logo"/>
             <FormHeader title="PLAN-IT PRO" />
-            <Form />
+            <Form onClick={handleClick}/>
             {/* <OtherMethods /> */}
             <p> Forgot Password?</p>
             <p> Don't have an account? <Link to= "/signup">Sign Up</Link></p>
@@ -36,13 +57,13 @@ const Form = props => (
    <span>
      <FormInput description="Username" placeholder="Enter your username" type="text" />
      <FormInput description="Password" placeholder="Enter your password" type="password"/>
-     <FormButton title="Log in"/>
+     <FormButton title="Log in" onClick={props.onClick}/>
    </span>
 );
 
 const FormButton = props => (
   <div id="button" class="row">
-    <button>{props.title}</button>
+    <button onClick={props.onClick}>{props.title}</button>
   </div>
 );
 
