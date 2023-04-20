3a5db1224d7e9fc623718e909088ea36071d18d3
naumanijazch <24100311@lums.edu.pk>
2023-04-20 22:40:25 +0500

    1.23

--- client/src/components/DashboardSubmitComplaint.js
+++ client/src/components/DashboardSubmitComplaint.js
@@ -48,18 +48,21 @@ const DashboardAddMoney = () => {
       <img className="top-bar-icon" alt="" src={topbar} />
       <img className="bg-01-1-icon" alt="" src={background} />
       <div className="dashboard-add-money-child" />
-      <b className="add-money">submitComplaints</b>
+      <b className="add-money4">Submit Complaints</b>
       <div className="dashboard-add-money-item" />
       <button className="dashboard-add-money-inner" />
 
       <div className="add-money1">
-        <Link
-          
-          style={{ textDecoration: "none", color: "white" }}
-          onClick={handleSaveChanges}
-        >
+        <button
+          style={{
+            textDecoration: "none",
+            color: "white",
+            backgroundColor: "transparent",
+            fontSize: "18px",
+          }}
+          onClick={handleSaveChanges}>
           Sumbit Complaint
-        </Link>
+        </button>
       </div>
 
       <div className="enter-the-amount"> 
@@ -71,9 +74,9 @@ const DashboardAddMoney = () => {
         src={logo}
       />
       <input
-        className="atom-input-container-sizes1"
+        className="atom-input-container-sizes2"
         type="text"
-        placeholder="Comments (Optional)"
+        placeholder="Type your Complaint here"
         value={comments}
         onChange={handleCommentsChange}
 
