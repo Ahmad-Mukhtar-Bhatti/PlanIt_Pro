13547e1606d989e0be3ca0ee48e0277bad51daa0
sikandaral <42680181+sikandaral@users.noreply.github.com>
2023-04-20 19:20:03 +0500

    packages

--- client/src/components/DashboardRemoveMoney.js
+++ client/src/components/DashboardRemoveMoney.js
@@ -20,7 +20,7 @@ const DashboardRemoveMoney = () => {
     alert(`Debited Money: ${amount}, type: ${type}`)
 
     try {
-      const response = await axios.post("http://localhost:3010/removemoney", {userID, amount});
+      const response = await axios.post("http://localhost:3010/removemoney", {userID, amount,type});
 
       console.log("removemoney",response)
       navigate('/home')
